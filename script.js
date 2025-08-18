6c4f51bd62f93c531e7849c7fd772708d8e63bc1
Rodrigo Yuji <rodrigo.y.seto@gmail.com>
2025-08-18 21:32:27 +0900

    feat: display temperature, wind speed and direction from API

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ document.querySelector(".search").addEventListener("submit", async (event) => {
   let input = document.querySelector("#searchInput").value;
 
   if (input !== "") {
+    clearInfo();
     showWarning("Loading...");
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
       input
-    )}&appid=${getCleanApiKey(
-      "fb9XcfYcaZdee3a87d587acb37e9a9a3d25"
+    )}&appid=${getApi(
+      "YZfZbX9XYcfXYcZaYZXdeZeXYZ3aXY87ZXdY5X8XZXXY7acXb3X7eXZX9XaZY9aZX3dYZX25Y"
     )}&units=metric`;
 
     let results = await fetch(url);
@@ -25,13 +26,33 @@ document.querySelector(".search").addEventListener("submit", async (event) => {
         windAngle: json.wind.deg,
       });
     } else {
+      clearInfo();
       showWarning("City not found");
     }
+  } else {
+    clearInfo();
   }
 });
 
 function showInfo(json) {
   showWarning("");
+
+  document.querySelector(".title").innerHTML = `${json.name}, ${json.country}`;
+  document.querySelector(".tempValue").innerHTML = `${json.temp} <sup>°C</sup>`;
+  document.querySelector(
+    ".windValue"
+  ).innerHTML = `${json.windSpeed} <span>km/h</span>`;
+
+  document
+    .querySelector(".temp img")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`
+    );
+
+  document.querySelector(".windPointer").style.transform = `rotate(${
+    json.windAngle - 90
+  }deg)`;
 
   document.querySelector(".result").style.display = "block";
 }
@@ -40,6 +61,11 @@ function showWarning(msg) {
   document.querySelector(".warning").innerHTML = msg;
 }
 
-function getCleanApiKey(rawKey) {
-  return rawKey.replace(/[XYZ]/g, "");
+function clearInfo() {
+  showWarning("");
+  document.querySelector(".result").style.display = "none";
+}
+
+function getApi(raw) {
+  return raw.replace(/[XYZ]/g, "");
 }
